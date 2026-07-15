dc5790f50a64ffbdb65dcaaa5bc6f8116c195bc5
geisabpa-a11y <geisabpa@gmail.com>
2026-07-14 19:51:32 -0600

    Fix missing script tag wrapper that caused JS content leak in HTML

--- views/admin_ejecutivos.pl
+++ views/admin_ejecutivos.pl
@@ -285,7 +285,8 @@ print <<HTML;
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2\@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2\@11"></script>
+
+<script>
     document.getElementById('form-ejecutivo').addEventListener('submit', function(e) {
         e.preventDefault();
         const fd = new FormData(this);
