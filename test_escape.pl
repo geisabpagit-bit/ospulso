277aae35451738b18d237c385d90d0a862af715c
geisabpa-a11y <geisabpa@gmail.com>
2026-08-04 21:11:11 -0600

    Clean up remaining test files

--- test_escape.pl
+++
@@ -1,6 +0,0 @@
-$_ = 'match this';
-/match/;
-print "Zero escaped: $'\n";
-print "One escaped: \$\'\n";
-print "Two escaped: \\$'\n";
-print "Three escaped: \\\$'\n";
--- test_wrapper.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-BEGIN {
-    package auth;
-    sub check_session { return { session_ok => 1, usuario => 'admin', role => 'Medico', id_medico => 'DOC-001' }; }
-    $INC{'../auth/check_session.pl'} = 1;
-}
-$ENV{QUERY_STRING} = "id=3&id_cita=1785898474";
-do './views/render_consultas_privado.pl';
