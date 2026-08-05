59d757d658daa9e8877f1735803410b43a69df39
geisabpa-a11y <geisabpa@gmail.com>
2026-08-04 20:49:42 -0600

    Clean up test files

--- test_wrapper.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-package auth;
-sub check_session { return { session_ok => 1, usuario => 'admin', role => 'Medico', id_medico => 'DOC-001' }; }
-$INC{'../auth/check_session.pl'} = 1;
-package main;
-$ENV{QUERY_STRING} = "id=1";
-do './views/render_consultas_privado.pl';
